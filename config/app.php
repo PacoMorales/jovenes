a199a00b125da47eca77192e25b72e4f9654eed9
PacoMorales <ErJavo_Zarate@hotmail.com>
2018-09-14 09:20:19 -0500

    DocPDF-Comentarios

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         Laracasts\Flash\FlashServiceProvider::class,
         Yajra\Oci8\Oci8ServiceProvider::class,
         //Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
+        //Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -222,7 +222,7 @@ return [
         'Html' => Collective\Html\HtmlFacade::class,
         'Flash'=> Laracast\Flash\Flash::class,
         'PDF'  => Barryvdh\DomPDF\Facade::class, 
-        'ConexionOracle' => Illuminate\Database\ConexionOracle\Conexion::class,
+        //'ConexionOracle' => Illuminate\Database\ConexionOracle\Conexion::class,
 
     ],
 
