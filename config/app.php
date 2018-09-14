d01ed053e590fc1d9017db69bae5912fb69ab7b3
PacoMorales <ErJavo_Zarate@hotmail.com>
2018-09-14 09:30:08 -0500

    DocPDF-EnComentarios

--- config/app.php
+++ config/app.php
@@ -221,8 +221,8 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Flash'=> Laracast\Flash\Flash::class,
-        'PDF'  => Barryvdh\DomPDF\Facade::class, 
-        //'ConexionOracle' => Illuminate\Database\ConexionOracle\Conexion::class,
+        //'PDF'  => Barryvdh\DomPDF\Facade::class, 
+        'ConexionOracle' => Illuminate\Database\ConexionOracle\Conexion::class,
 
     ],
 
